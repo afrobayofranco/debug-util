4f01ffe8e253989912ecf112f91c9efea5c5ea52
sdvinov <odvakr.wikia@gmail.com>
2016-10-13 23:32:55 -0400

    Done with functionality

---
+++ test/__debug.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+const log = require('../src/index');
+
+describe('Debugger Test', () => {
+  it('Should save to file', (done) => {
+    expect(log.debug).to.not.equal(null);
+    done();
+  });
+});
